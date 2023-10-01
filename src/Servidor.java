2370bc6ef950e3eeb10fff6cba9ab4df6fe190bb
Hector Alonso Caravaca Vargas <hcaravacavargas@gmail.com>
2023-10-01 09:03:58 -0600

    Receive message to Cliente.java

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,17 +1,19 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import menu.Product;
 
-// ESTE ES EL RESTAURANTE
-
 public class Servidor {
+    public static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         int puerto = 12345;
-
+        ServerSocket servidorSocket = null;
+        
         try {
-            ServerSocket servidorSocket = new ServerSocket(puerto);
+            servidorSocket = new ServerSocket(puerto);
             System.out.println("Servidor esperando conexiones en el puerto " + puerto);
 
             while (true) {
@@ -20,31 +22,69 @@ public class Servidor {
 
                 // Configurar flujos de entrada y salida
                 ObjectInputStream entradaObjeto = new ObjectInputStream(clienteSocket.getInputStream());
+                BufferedReader entrada = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+                PrintWriter confirmacion = new PrintWriter(clienteSocket.getOutputStream(), true);
 
                 // Recibir la lista de productos seleccionados desde el cliente
-                ArrayList<Product> productosSeleccionados = (ArrayList<Product>) entradaObjeto.readObject(); //PUEDE HABER UN ERROR POR ACA
+                ArrayList<Product> productosSeleccionados = (ArrayList<Product>) entradaObjeto.readObject();
 
                 // Procesar los productos seleccionados
                 System.out.println("Productos seleccionados por el cliente:");
                 for (Product producto : productosSeleccionados) {
-                    System.out.println("Nombre: " + producto.getName() + ", Precio: " + producto.getPrice());
-                    // Realiza cualquier otra lógica de procesamiento que necesites aquí
+                    System.out.println("Nombre: " + producto.getName() + " - Precio: " + producto.getPrice() + " - cantidad: " + producto.getCantidad());
                 }
 
-                // Enviar una confirmación al cliente
-                PrintWriter confirmacion = new PrintWriter(clienteSocket.getOutputStream(), true);
-                confirmacion.println("Pedido recibido con éxito");
+                while (true) {
+                    // Enviar un mensaje al cliente
+                    confirmacion.println("1. Comer acá? 2. Para llevar 3. Servicio Express 4. No Opcion: ");
 
-                // Cerrar la conexión con el cliente
-                clienteSocket.close();
+                    // Leer la respuesta del cliente
+                    String respuestaCliente = entrada.readLine();
 
-                // Cerrar la conexion con el servidor
-                servidorSocket.close();
+                    if (respuestaCliente != null && !respuestaCliente.isEmpty()) {
+                        try {
+                            int opcion = Integer.parseInt(respuestaCliente);
+                            switch (opcion) {
+                                case 1:
+                                    confirmacion.println("El cliente eligió comer acá");
+                                    break;
+                                case 2:
+                                    confirmacion.println("El cliente eligió para llevar");
+                                    break;
+                                case 3:
+                                    confirmacion.println("El cliente eligió servicio express");
+                                    break;
+                                case 4:
+                                    confirmacion.println("El cliente eligió no");
+                                    break;
+                                default:
+                                    confirmacion.println("El cliente eligió una opción no válida");
+                                    break;
+                            }
+                        } catch (NumberFormatException e) {
+                            // Maneja el error si 'respuestaCliente' no es un número válido
+                            confirmacion.println("El cliente envió una opción no válida.");
+                        }
+                    } else {
+                        // Maneja el caso en el que la respuesta esté vacía o sea nula
+                        confirmacion.println("El cliente envió una respuesta vacía o nula.");
+                    }
+
+                    clienteSocket.close();
+                }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("El cliente ha perdido la conexión");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (servidorSocket != null) {
+                    servidorSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
