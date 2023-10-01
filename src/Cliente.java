3abd48ee191f22b085be4e22b5cc967ee9584fda
Hector Alonso Caravaca Vargas <hcaravacavargas@gmail.com>
2023-10-01 09:03:01 -0600

    Add cantidad, send message to Servidor class, exception handling

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import menu.Menu;
 import menu.Product;
@@ -8,7 +9,7 @@ import menu.Product;
 // ESTA ES LA APP QUE DEBE MANDAR LAS COSAS AL SERVER
 public class Cliente {
     public static Scanner input = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void mandarInformacion() throws IOException, InputMismatchException {
         String servidorIP = "127.0.0.1";
         int puerto = 12345;
 
@@ -18,13 +19,15 @@ public class Cliente {
 
             // Configurar flujos de entrada y salida
             BufferedReader entrada = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+            PrintWriter salida = new PrintWriter(clienteSocket.getOutputStream(), true);
+
             ObjectOutputStream salidaObjeto = new ObjectOutputStream(clienteSocket.getOutputStream());
 
             // Mostramos el menu ya creado
             Menu menu = new Menu();
-            menu.appendProduct(new Product("Producto 1", "Este es el producto 1", 10.0, "Almuerzos", 10));
-            menu.appendProduct(new Product("Producto 2", "Este es el producto 2", 20.0, "Almuerzos", 20));
-            menu.appendProduct(new Product("Producto 3", "Este es el producto 3", 30.0, "Almuerzos", 30));
+            menu.appendProduct(new Product("Producto 1", "Este es el producto 1", 10.0, "Almuerzos", 10, 1));
+            menu.appendProduct(new Product("Producto 2", "Este es el producto 2", 20.0, "Almuerzos", 20, 1));
+            menu.appendProduct(new Product("Producto 3", "Este es el producto 3", 30.0, "Almuerzos", 30, 1));
 
             // Enviar el primer string (identificador del pedido) al servidor
             ArrayList<Product> seleccionados = new ArrayList<>(); // GUARDAR LO ELEGIDO POR EL CLIENTE;
@@ -32,7 +35,7 @@ public class Cliente {
             System.out.println("Menú:");
             int i = 1; // Variable para efectos visuales
             for (Product product : menu.getProducts()) {
-                System.out.println(i + ". Name:" + product.getName() + ", category: " + product.getCategory() + " - $" + product.getPrice());
+                System.out.println(i + ". " + product.toString());
                 i++;
             }
 
@@ -44,10 +47,17 @@ public class Cliente {
                     break; // El cliente ha terminado de seleccionar
 
                 } else if (opcion >= 1 && opcion <= menu.getProducts().size()) { // Tamaño del menu de los platillos
-
                     Product platilloElegido = menu.getProducts().get(opcion - 1);
-                    seleccionados.add(platilloElegido); // Se agrega la opcion a la lista de los platillos
-                    System.out.println("Ha seleccionado: " + platilloElegido.getName() + " - $" + platilloElegido.getPrice());
+                    System.out.println("Cuantos quiere?");
+                    int cantidad = input.nextInt();
+                    if (cantidad == 0) {
+                        System.out.println("No se puede pedir 0 platillos");
+                        continue;
+                    } else {
+                        platilloElegido.setCantidad(cantidad);
+                        seleccionados.add(platilloElegido); // Se agrega la opcion a la lista de los platillos
+                        System.out.println("Ha seleccionado: " + platilloElegido.getName() + " - $" + platilloElegido.getPrice());
+                    }
                 } else {
                     System.out.println("Opción no válida.");
                 }
@@ -56,14 +66,46 @@ public class Cliente {
             // Enviar la lista de productos seleccionados al servidor
             salidaObjeto.writeObject(seleccionados);
 
-            String confirmacion = entrada.readLine();
-            System.out.println("Servidor: " + confirmacion);
+            try {
+                while (true) {
+                    // Leer el mensaje del servidor
+                    String mensajeServidor = entrada.readLine();
+                    if (mensajeServidor == null) {
+                        break; // El servidor cerró la conexión
+                    }
+                    System.out.println("Servidor: " + mensajeServidor);
+
+                    // Enviar un mensaje al servidor
+                    System.out.print("Cliente: ");
+                    String mensajeCliente = input.nextLine();
+                    salida.println(mensajeCliente);
+                }
+            } catch (IOException e) {
+                System.out.println("El servidor ha cerrado la conexión.");
+            }
+
 
             // Cerrar la conexión con el servidor
             clienteSocket.close();
+
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InputMismatchException ime) {
+            System.out.println("Error ! Debe ingresar un numero entero !");;
         }
+    }
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("PLEASE PRESS ENTER");
+            input.nextLine(); // LIMPIAR BUFFER PARA NO ENTRAR EN UN CICLO INFINITO, PRESIONE ENTER Y CONTINUA
+            mandarInformacion();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InputMismatchException ime) {
+            System.out.println("Error: Debe ingresar un número entero");
+        }
+
     }
 }
 
